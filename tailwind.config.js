f7e6b362e8d36977a9893c7c937624fd2ea779fb
Фархат Тохтиев <agrotradegroupturkiye@gmail.com>
2025-12-08 20:03:18 +0300

    Добавлен safelist в tailwind.config.js для корректного отображения цветов

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./styles/**/*.{css}"
   ],
+  safelist: [
+    'bg-blue-100', 'text-blue-700',
+    'bg-green-100', 'text-green-700',
+    'bg-yellow-100', 'text-yellow-700',
+    'bg-purple-100', 'text-purple-700',
+  ],
   theme: {
     extend: {
       colors: {
